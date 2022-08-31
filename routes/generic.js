6b968eb47ab9ab0ef0bab1c8775d631e976d2ff0
NicolleAmr <86444373+NicolleAmr@users.noreply.github.com>
2022-08-31 12:24:58 -0300

    Add files via upload

--- routes/generic.js
+++ routes/generic.js
@@ -1,5 +1,5 @@
 module.exports = (app)=>{
-    app.get("/elements",(req,res)=>(
-        res.render('elements.ejs')
-    )) 
+    app.get("/generic",(req,res)=>{
+        res.render('generic.ejs')
+    }) 
 }
